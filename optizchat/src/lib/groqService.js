ecfa2fe5a02455aad011b23809b6a97fb5d2bd22
etrossudio <etrossudio@gmail.com>
2024-11-22 20:14:21 +0000

    will be fixed

--- optizchat/src/lib/groqService.js
+++ optizchat/src/lib/groqService.js
@@ -1,24 +1,24 @@
 import Groq from "groq-sdk";
 
 const groq = new Groq({
-  apiKey: import.meta.env.VITE_GROQ_API_KEY,
-  dangerouslyAllowBrowser: true,
+    apiKey: import.meta.env.VITE_GROQ_API_KEY,
+    dangerouslyAllowBrowser: true,
 });
 
-export async function getGroqChatStream(userMessage, model) {
-  return groq.chat.completions.create({
-    messages: [
-      {
-        role: "system",
-        content: "you are a helpful assistant.",
-      },
-      {
-        role: "user",
-        content: userMessage,
-      },
-    ],
-    model: model,
-    stream: true,
-    // Other parameters...
-  });
-} 
+export const getGroqChatCompletion = async (prompt, model = "llama3-8b-8192") => {
+    return groq.chat.completions.create({
+        messages: [
+            {
+                role: "system",
+                content: "You are a smart AI that checks and thinks two times before answering.",
+            },
+            { role: "user", content: prompt },
+        ],
+        model: model, 
+        temperature: 0.5,
+        max_tokens: 1024,
+        top_p: 1,
+        stop: null,
+        stream: false,
+    });
+};
